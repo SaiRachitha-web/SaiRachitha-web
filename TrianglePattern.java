7242d3197e6b83a3c1f5d6f4dcd65936172cc3b7
SaiRachitha-web <polurusairachitha@gmail.com>
2025-12-19 08:29:31 +0530

    Add files via upload

---
+++ TrianglePattern.java
@@ -0,0 +1,8 @@
+public class TrianglePattern {
+    public static void main(String[] args) {
+        System.out.println("      *");
+        System.out.println("     * *");
+        System.out.println("    *   *");
+        System.out.println("   *******");
+    }
+}
